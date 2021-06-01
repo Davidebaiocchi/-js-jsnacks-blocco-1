2d6febf473f4ac504de89e4479aea9d196bde664
davide <davide.baiocchi97@gmail.com>
2021-06-01 13:51:36 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// JSnack 1
+// L’utente inserisce due parole in successione, con due prompt. 
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+
+
+// Jsnack2
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+// Il programma stampa la somma di tutti i numeri inseriti. 
+// Esegui questo programma in due versioni, con il for e con il while.
+
+
+
+
+// Jsnack3
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+
+
+
+
+// Jsnack4
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby.
+// Chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
